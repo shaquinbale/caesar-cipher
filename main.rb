3a90485d35059ebd53e1c6af7df3d84ff7c17e4e
shaquinbale <scopelenskingdra@gmail.com>
2024-11-02 22:14:38 -0400

    Submit my code to the rubocop police

--- main.rb
+++ main.rb
@@ -1,17 +1,20 @@
-def shift_letter (char, shift)
+require 'bundler/setup'
+require 'rubocop'
+
+def shift_letter(char, shift)
   shifted_ord = char.ord + shift
 
   if char =~ /[a-z]/
-    return shifted_ord > 122 ? (shifted_ord - 26).chr : shifted_ord.chr 
+    shifted_ord > 122 ? (shifted_ord - 26).chr : shifted_ord.chr
   elsif char =~ /[A-Z]/
-    return shifted_ord > 90 ? (shifted_ord - 26).chr : shifted_ord.chr 
+    shifted_ord > 90 ? (shifted_ord - 26).chr : shifted_ord.chr
   else
-    return char
+    char
   end
 end
 
-def caesar_cipher (string, shift)
-  arrayed_answer = string.chars.map {|char| shift_letter(char, shift)}
+def caesar_cipher(string, shift)
+  arrayed_answer = string.chars.map { |char| shift_letter(char, shift) }
   arrayed_answer.join
 end
 
